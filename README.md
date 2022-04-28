02518ed588a35ae7503dda8b910edc5e773979d1
Patrick Basamot <patrickbasamot@gmail.com>
2022-04-28 18:43:36 +0800

    added the starting files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Meme-Scraper
+
+A project to retrieve memes from different subreddits 
+a Web scraper API made by > @D3vd
+[Github Repo](https://github.com/D3vd/Meme_Api);;
---
+++ script.js
@@ -0,0 +1,36 @@
+const subreddit = document.querySelectorAll('.subreddit');
+let redditMemeWebScraper = `https://meme-api.herokuapp.com/gimme/5`;
+
+
+
+fetchMeme(redditMemeWebScraper);
+
+
+
+subreddit.forEach(btn =>{
+    btn.addEventListener('click',function choose(){
+        redditMemeWebScraper = `https://meme-api.herokuapp.com/gimme/${btn.textContent.toLowerCase()}/5`;
+        fetchMeme(redditMemeWebScraper);
+    })
+})
+
+
+
+async function fetchMeme(redditMemeWebScraper){
+        let response = await fetch(redditMemeWebScraper);
+        let data = await response.json();
+        console.log(data);
+        renderData(data);
+}
+
+
+    function renderData(data){
+      const { memes } = data;
+      memes.forEach(rowofMemes => {
+         const {author,postLink,preview,title,subreddit} = rowofMemes;
+         let imgUrl = preview.slice(-1);
+         let strUlr = imgUrl.toString();
+         console.log(strUlr);
+      })
+    }
+
