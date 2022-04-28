3b0e4b79873be0729841c013d359f9ccc380261b
Patrick Basamot <patrickbasamot@gmail.com>
2022-04-28 20:44:12 +0800

    edits

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 A project to retrieve memes from different subreddits 
 a Web scraper API made by > @D3vd
-\n
+
 [Github Repo](https://github.com/D3vd/Meme_Api);
--- script.js
+++ script.js
@@ -17,6 +17,7 @@ subreddit.forEach(btn =>{
 
 
 async function fetchMeme(redditMemeWebScraper){
+    
         let response = await fetch(redditMemeWebScraper);
         let data = await response.json();
         console.log(data);
